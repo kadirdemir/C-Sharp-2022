518b8faec883d747f28e9eaceb78c2dff6734e13
kadirdemir <kademir07@gmail.com>
2022-03-24 08:31:18 +0300

    Kucuk Turun Buyuk Ture donusumu eklendi

---
+++ Tür Dönüşümleri/kucuk_turun_buyuk_ture/kucuk_turun_buyuk_ture/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class kucuk_turun_buyuk_ture
+{
+    static void Main()
+    {
+        byte a = 20;
+        int b;
+        b = a;
+        Console.WriteLine(b);
+
+        float f = 20f;
+        double d;
+        d = f;
+        Console.WriteLine(d);
+
+        char c = 'a';
+        decimal m;
+        m = c;
+        Console.WriteLine(m);
+    }
+}
