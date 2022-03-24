2ed27711b4364946a74a8114159f2fc0292f4cd9
kadirdemir <kademir07@gmail.com>
2022-03-24 08:38:32 +0300

    Farkli Turden Degiskenler Ayni Ifadeye Aktarildi

---
+++ Tür Dönüşümleri/farklı_tur_nesneler_ayni_ifadeye/farklı_tur_nesneler_ayni_ifadeye/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class farklı_tur_aynı_ifade
+{
+    static void Main()
+    {
+        int a = 10;
+        byte b = 20;
+        short s = 30;
+        double d;
+        d = a + b + s;
+        Console.WriteLine(d);
+    }
+}
