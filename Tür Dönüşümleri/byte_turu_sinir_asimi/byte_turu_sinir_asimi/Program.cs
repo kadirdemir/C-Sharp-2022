8b09adc80e5dae098643386338412ef7fc85fff3
Kadir Demir <kademir07@gmail.com>
2022-03-30 16:10:37 +0300

    byte turu sinir asimi eklendi

---
+++ Tür Dönüşümleri/byte_turu_sinir_asimi/byte_turu_sinir_asimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class byte_turu_sinir_asimi
+{
+    static void Main()
+    {
+        byte a = 16;
+        byte b = 200;
+        byte c = a * b;
+        //byte yerine int yazılırsa program sorunsuz derlenir ve çalışır
+        Console.WriteLine(c);
+    }
+}
