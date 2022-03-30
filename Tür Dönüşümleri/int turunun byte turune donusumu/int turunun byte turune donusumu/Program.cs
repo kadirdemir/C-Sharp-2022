f28d3512b4fdfe3fa453fae26545d88030b6a067
Kadir Demir <kademir07@gmail.com>
2022-03-30 15:50:06 +0300

    int turunun byte turune donusumu

---
+++ Tür Dönüşümleri/int turunun byte turune donusumu/int turunun byte turune donusumu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class TurDonusumu4
+{
+    static void Main()
+    {
+        byte a = 5;
+        byte b = 10;
+        byte c = a + b;
+        //byte c = a + b; ifadesinde byte yerine int yazılırsa hata düzelir
+        //cannot implicitly convert type int to byte
+        //int türü byte türüne bilincsiz olarak dönüstürülemedi
+        Console.WriteLine(c);
+    }
+}
