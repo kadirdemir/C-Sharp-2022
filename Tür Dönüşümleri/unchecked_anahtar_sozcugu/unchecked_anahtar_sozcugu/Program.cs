70df603b68da3b13389e887b81d16ba3ae903c84
kadirdemir <kademir07@gmail.com>
2022-03-30 23:25:35 +0300

    Unchecked anahtar sözcüğü eklendi

---
+++ Tür Dönüşümleri/unchecked_anahtar_sozcugu/unchecked_anahtar_sozcugu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class unchecked_anahtar_sozcugu
+{
+    static void Main()
+    {
+        int i = 256;
+        int a = 300;
+        byte b, c;
+        checked
+        {
+            b = (byte)i;
+
+            unchecked
+            {
+                c = (byte)a;
+            }
+        }
+        Console.WriteLine(b);
+    }
+}
