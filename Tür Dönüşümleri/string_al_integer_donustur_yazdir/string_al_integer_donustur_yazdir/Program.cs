7db02c4bd05acf99a391492f12fbc44c27aa966a
kadirdemir <kademir07@gmail.com>
2022-03-31 00:54:20 +0300

    String alınan ifadelerin Int türüne dönüştürülerek ekrana yazdırılması eklendi

---
+++ Tür Dönüşümleri/string_al_integer_donustur_yazdir/string_al_integer_donustur_yazdir/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class string_al_integer_donustur_yazdir
+{
+    static void Main()
+    {
+        string s1, s2;
+        int sayi1, sayi2;
+        int Toplam;
+
+        Console.WriteLine("İlk sayıyı gir:");
+        s1 = Console.ReadLine();
+
+        Console.WriteLine("İkinci sayıyı gir:");
+        s2 = Console.ReadLine();
+
+        sayi1=Convert.ToInt32(s1);
+        sayi2=Convert.ToInt32(s2);  
+        Toplam = sayi1 + sayi2;
+        Console.WriteLine("Toplam =" + Toplam.ToString());
+
+    }
+}
