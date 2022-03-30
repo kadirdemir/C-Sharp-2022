75ab7f96043dbfa5a03caf58059282d70a313fb4
kadirdemir <kademir07@gmail.com>
2022-03-30 22:59:50 +0300

    Checked anahtar sözcüğü eklendi

---
+++ Tür Dönüşümleri/checked_anahtar_sozcugu/checked_anahtar_sozcugu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class checked_anahtar_sozcugu
+{
+    static void Main()
+    {
+        int i = 256;
+        byte b;
+        checked
+        {
+            b = (byte)i;
+        }
+        Console.WriteLine(b);
+    }
+}
