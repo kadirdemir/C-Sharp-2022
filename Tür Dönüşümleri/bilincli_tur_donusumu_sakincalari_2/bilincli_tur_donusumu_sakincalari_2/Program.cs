ad839095fbb5ff054dc0893327906fea0860417f
kadirdemir <kademir07@gmail.com>
2022-03-30 22:49:39 +0300

    Bilinçli tür dönüşümü sakıncaları (veri kaybı örneği) eklendi.

---
+++ Tür Dönüşümleri/bilincli_tur_donusumu_sakincalari_2/bilincli_tur_donusumu_sakincalari_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class bilincli_tur_donusumu_sakincalari_2
+{
+    static void Main()
+    {
+        int i = 256;
+        byte b = (byte)i;
+        Console.WriteLine(b);
+    }
+}
