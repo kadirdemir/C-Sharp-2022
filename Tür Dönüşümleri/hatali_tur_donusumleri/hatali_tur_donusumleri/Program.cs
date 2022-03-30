19f5bb3cac6f176c1d81a2963d9b05fc748342bb
kadirdemir <kademir07@gmail.com>
2022-03-30 20:24:25 +0300

    Hatalı tür dönüşümleri eklendi

---
+++ Tür Dönüşümleri/hatali_tur_donusumleri/hatali_tur_donusumleri/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class hatali_tur_donusumleri
+{
+    static void Main()
+    {
+        short b1 = 50;
+        char c1 = b1;
+        bool b2 = true;
+        int i1 = b2;
+        double d1 = 10.2;
+        int i2 = d1;
+        decimal m1 = 25.4M;
+        double d2 = m1;
+        byte bt1 = 65;
+        char c2 = bt1;
+        float f1 = 25.74F;
+        decimal d3 = f1;
+    }
+}
