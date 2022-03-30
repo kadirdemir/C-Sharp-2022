d4fb94f968eb3682928768289f3fb843cb537857
kadirdemir <kademir07@gmail.com>
2022-03-30 23:05:28 +0300

    Checked anahtar sözcüğü blok faaliyet alanı eklendi

---
+++ Tür Dönüşümleri/checked_anahtar_sozcugu_blok/checked_anahtar_sozcugu_blok/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class checked_anahtar_sozcugu_blok
+{
+    static void Main()
+    {
+        int i = 256;
+
+        checked
+        {
+            byte b = (byte)i;
+        }
+        Console.WriteLine(b);
+    }
+}
