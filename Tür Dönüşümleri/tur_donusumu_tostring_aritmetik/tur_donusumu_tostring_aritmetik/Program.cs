a18d7e1813d9dd21a4a7e0deba653dd5d3c95dee
kadirdemir <kademir07@gmail.com>
2022-03-30 23:35:41 +0300

    Tür dönüşümü ToString( ) metodu aritmetik işlem eklendi

---
+++ Tür Dönüşümleri/tur_donusumu_tostring_aritmetik/tur_donusumu_tostring_aritmetik/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class tur_donusumu_tostring_aritmetik
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 7;
+        string a1 = a.ToString();
+        string b1 = b.ToString();
+        Console.WriteLine(a + b);
+        Console.WriteLine(a1 + b1);
+    }
+}
