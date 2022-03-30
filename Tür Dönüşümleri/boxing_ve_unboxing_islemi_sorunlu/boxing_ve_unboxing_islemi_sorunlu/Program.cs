71ccaf7cf7c769bd8c1fb17ce72c22cdb38d936b
kadirdemir <kademir07@gmail.com>
2022-03-31 00:29:01 +0300

    Boxing ve Unboxing işlemi sorunlu eklendi

---
+++ Tür Dönüşümleri/boxing_ve_unboxing_islemi_sorunlu/boxing_ve_unboxing_islemi_sorunlu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class boxing_ve_unboxing_islemi_sorunlu
+{
+    static void Main()
+    {
+        int i = 10;
+        object o = i;
+
+        int j = (byte)o;
+
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+    }
+}
