02b428e5b2ea5e9a9000ae3fd51d005a52d255e3
kadirdemir <kademir07@gmail.com>
2022-03-31 00:21:29 +0300

    Boxing ve Unboxing işlemi sorunsuz eklendi

---
+++ Tür Dönüşümleri/boxing_ve_unboxing_islemi_sorunsuz/boxing_ve_unboxing_islemi_sorunsuz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class boxing_ve_unboxing_islemi_sorunsuz
+{
+    static void Main()
+    {
+        int i = 10;
+        object o = i;
+
+        int j = (int)o;
+
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+    }
+}
