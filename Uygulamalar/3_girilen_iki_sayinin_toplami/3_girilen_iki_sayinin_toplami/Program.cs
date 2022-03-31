cb46396829d3d43322bda975340e4a805855a0f7
Kadir Demir <kademir07@gmail.com>
2022-03-31 12:20:30 +0300

    İki sayının toplamı eklendi

---
+++ Uygulamalar/3_girilen_iki_sayinin_toplami/3_girilen_iki_sayinin_toplami/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+
+	static void Main()
+	{
+		int s1, s2;
+		int topla;
+
+		Console.Write("İlk sayıyı giriniz: ");
+		s1 = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("İkinci sayıyı giriniz: ");
+		s2 = Convert.ToInt32(Console.ReadLine());
+
+		topla = (s1 + s2);
+
+		Console.WriteLine("İki sayı toplamı: " + topla);
+		Console.Read();
+	}
+}
