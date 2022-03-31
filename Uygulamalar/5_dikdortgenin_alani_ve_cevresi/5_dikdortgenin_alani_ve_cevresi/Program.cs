a953445fea824cffc49bea12f9699081eb905290
Kadir Demir <kademir07@gmail.com>
2022-03-31 13:17:55 +0300

    Dikdörtgenin alanı ve çevresi eklendi

---
+++ Uygulamalar/5_dikdortgenin_alani_ve_cevresi/5_dikdortgenin_alani_ve_cevresi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class dikdortgenin_alani_ve_cevresi
+{
+	static void Main()
+	{
+		double g, y, a, c;
+
+		Console.Write("Genişliği giriniz: ");
+		g = Convert.ToDouble(Console.ReadLine());
+
+		Console.Write("Yüksekliği giriniz: ");
+		y = Convert.ToDouble(Console.ReadLine());
+
+		a = g * y;
+		c = 2 * g + 2 * y;
+
+		Console.WriteLine("Alanı = " + a + " ve Çevresi = " + c);
+		Console.Read();
+	}
+}
