c15cd39dd708265733fc05a8347e087694cbebb0
Kadir Demir <kademir07@gmail.com>
2022-03-31 11:53:32 +0300

    Girilen sayının karesi eklendi

---
+++ Uygulamalar/2_girilen_sayinin_karesi/2_girilen_sayinin_karesi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class girilen_sayinin_karesi
+{
+    static void Main()
+    {
+        int sayi, kare;
+
+        Console.WriteLine("Sayı gir:");
+
+        sayi = Convert.ToInt32(Console.ReadLine());
+
+        kare = sayi * sayi;
+
+        Console.Write(("Sayının karesi:" + kare));
+
+        Console.Read();
+    }
+}
