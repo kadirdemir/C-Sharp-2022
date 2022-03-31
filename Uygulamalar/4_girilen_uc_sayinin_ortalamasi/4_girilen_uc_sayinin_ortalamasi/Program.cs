218d85edf6d19091ba43e32dbabdbd74f5d8ed16
Kadir Demir <kademir07@gmail.com>
2022-03-31 13:08:45 +0300

    Üç sayının ortalaması eklendi

---
+++ Uygulamalar/4_girilen_uc_sayinin_ortalamasi/4_girilen_uc_sayinin_ortalamasi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+
+	static void Main()
+	{
+		int s1, s2, s3, ort;
+
+		Console.Write("İlk sayıyı giriniz: ");
+		s1 = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("İkinci sayıyı giriniz: ");
+		s2 = Convert.ToInt32(Console.ReadLine());
+		
+		Console.Write("Üçüncü sayıyı giriniz: ");
+		s3 = Convert.ToInt32(Console.ReadLine());
+		
+		ort = (s1 + s2 + s3) / 3;
+
+		Console.WriteLine("Üç sayının ortalaması: " + ort);
+		Console.Read();
+	}
+}
