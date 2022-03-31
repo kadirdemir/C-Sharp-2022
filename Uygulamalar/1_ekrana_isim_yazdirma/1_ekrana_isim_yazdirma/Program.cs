17bf083f2abcab1bd79b2d2b0656c478ab1dcfe2
Kadir Demir <kademir07@gmail.com>
2022-03-31 11:52:29 +0300

    Ekrana isim yazdirma eklendi

---
+++ Uygulamalar/1_ekrana_isim_yazdirma/1_ekrana_isim_yazdirma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class ekrana_isim_yazdirma
+{
+
+    static void Main()
+    {
+        string ad;
+        Console.Write("Adınızı yazınız:");
+        ad = Console.ReadLine();
+        Console.WriteLine("Merhaba " + ad);
+        Console.ReadLine();
+    }
+}
