b95279e55f808605c974faeba92e86b319784d05
kadirdemir <kademir07@gmail.com>
2022-04-06 23:13:31 +0300

    Bitsel VE Operatörü eklendi

---
+++ Operatörler/bitsel_ve_operatoru/bitsel_ve_operatoru/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Bitsel_ve_operatoru
+{
+    static void Main()
+    {
+        byte a = 145;
+        byte b = 73;
+
+        int c = (byte)(a & b);
+        Console.WriteLine(c);
+    }
+}
