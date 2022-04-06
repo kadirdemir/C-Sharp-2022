804704ce6ad4eec84ca1fb1d504c92e8a7bafb71
kadirdemir <kademir07@gmail.com>
2022-04-07 00:27:34 +0300

    Bitsel Sola Kaydırma eklendi

---
+++ Operatörler/bitsel_sola_kaydirma/bitsel_sola_kaydirma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Bitsel_sola_kaydirma
+{
+    static void Main()
+    {
+        byte b = 10;
+        byte c = (byte)(b << 1);
+
+        Console.WriteLine("b: " + b);
+        Console.WriteLine("c: " + c);
+    }
+}
