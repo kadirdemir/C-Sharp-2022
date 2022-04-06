05dfbdabfb494efff1c2d411c864720bbf8da42d
kadirdemir <kademir07@gmail.com>
2022-04-07 00:49:23 +0300

    Ternary Operatörü eklendi

---
+++ Operatörler/ternary_operatoru/ternary_operatoru/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Ternary_operatoru
+{
+    static void Main()
+    {
+        int sayi;
+        Console.Write("Kalem sayısını giriniz: ");
+        sayi=Convert.ToInt32(Console.ReadLine());
+        string str = "kalem";
+        str = str + (sayi == 1 ? " " : "ler");
+        Console.WriteLine(str);
+    }
+}
