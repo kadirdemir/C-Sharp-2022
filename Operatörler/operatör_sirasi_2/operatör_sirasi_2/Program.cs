934ea1a173c2be1a9654618ec4037998bd52303e
kadirdemir <kademir07@gmail.com>
2022-04-06 20:48:15 +0300

    Operatör Sırası 2 eklendi

---
+++ Operatörler/operatör_sirasi_2/operatör_sirasi_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Operatör_sirasi_2
+{
+    static void Main()
+    {
+        int c = (2 + (9 + 2) * 5) * 2;
+        Console.WriteLine("C = (2 + (9 + 2) * 5) * 2 = " + c);
+
+        int d = 2 + 9 + 2 * 5 * 2;
+        Console.WriteLine("D = 2 + 9 + 2 * 5 * 2 = " + d);
+
+        int e = 2 + (9 + 2 * 5) * 2;
+        Console.WriteLine("E = 2 + (9 + 2 * 5) * 2 = " + e);
+    }
+}
