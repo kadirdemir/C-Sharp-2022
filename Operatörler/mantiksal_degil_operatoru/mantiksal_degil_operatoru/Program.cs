a4a78b4de348260d3c4b786452747eaaa3c4c2c5
kadirdemir <kademir07@gmail.com>
2022-04-06 22:49:37 +0300

    Mantıksal Değil Operatörü eklendi

---
+++ Operatörler/mantiksal_degil_operatoru/mantiksal_degil_operatoru/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Mantiksal_degil_operatoru
+{
+    static void Main()
+    {
+        bool b1 = !(35 > 10);
+        bool b2 = !(10 == 50);
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+
+    }
+}
