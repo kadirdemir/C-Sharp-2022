433db390ba1bad9ab0009c8bbf10ac0e5c1e8392
kadirdemir <kademir07@gmail.com>
2022-04-07 00:52:35 +0300

    typeof Operatörü eklendi

---
+++ Operatörler/typeof_operatoru/typeof_operatoru/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Typeof_operatoru
+{
+    static void Main()
+    {
+        Type t1 = typeof(Typeof_operatoru);
+        Typeof_operatoru den = new Typeof_operatoru();
+
+        Type t2 = den.GetType();
+
+        Console.WriteLine(t1.ToString());
+        Console.WriteLine(t2.ToString());
+    }
+}
