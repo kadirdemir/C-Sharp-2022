71405a7428c1cb8c9d3ffbee3eb076cde0e01c5f
kadirdemir <kademir07@gmail.com>
2022-04-06 15:49:35 +0300

    Tek Bilinmeyenli Denklem Çözümü eklendi

---
+++ Uygulamalar/9_tek_bilinmeyenli_denklem_cozumu/9_tek_bilinmeyenli_denklem_cozumu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Tek_bilinmeyenli_denklem_cozumu
+{
+    static void Main()
+    {
+        //ax+b=0 şeklindeki tek bilinmeyenli denklem çözümü
+        //bilinmeyen x
+        double a, b, x;
+
+        Console.WriteLine("ax + b = 0 şeklindeki tek bilinmeyenli denklem çözümü");
+
+        Console.Write("a: ");
+
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        x = -b / a;
+
+        Console.Write("x= " + x);
+
+        Console.ReadLine();
+    }
+}
