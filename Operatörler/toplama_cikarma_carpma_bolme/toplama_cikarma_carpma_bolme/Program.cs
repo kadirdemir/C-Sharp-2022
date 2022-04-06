48128ea33a0ec9756694ed7cec84f5b29960185f
kadirdemir <kademir07@gmail.com>
2022-04-06 21:13:24 +0300

    Toplama, Çıkarma, Çarpma ve Bölme eklendi

---
+++ Operatörler/toplama_cikarma_carpma_bolme/toplama_cikarma_carpma_bolme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Toplama_cikarma_carpma_bolme
+{
+    static void Main()
+    {
+        int i = 50 + 40;
+        Console.WriteLine(i);
+
+        float j = 12.56f + 11.4f;
+        Console.WriteLine(j);
+
+        int k = 50 * 40;
+        Console.WriteLine(k);
+
+        int n = 50 / 40;
+        Console.WriteLine(n);
+
+        float t = 50f / 40f;
+        Console.WriteLine(t);
+    }
+}
