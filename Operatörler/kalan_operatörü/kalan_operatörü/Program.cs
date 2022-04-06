b44be3198f636d9a4d3d9d0b9423c0798349ddc3
kadirdemir <kademir07@gmail.com>
2022-04-06 21:26:21 +0300

    Kalan Operatörü eklendi

---
+++ Operatörler/kalan_operatörü/kalan_operatörü/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Kalan_operatörü
+{
+    static void Main()
+    {
+        int c = 10 % 3;
+        Console.WriteLine(c);
+
+        int d = -7 % 4;
+        Console.WriteLine(d);
+
+        decimal e = 2.5m % 1.2m;
+        Console.WriteLine(e);
+
+        float f = 2.5f % 1.2f;
+        Console.WriteLine(f);
+    }
+}
