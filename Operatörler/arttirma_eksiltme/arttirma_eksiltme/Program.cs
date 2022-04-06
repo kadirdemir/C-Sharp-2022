4ed39e0d1b83e18d72559463fc330f32c0e80f4b
kadirdemir <kademir07@gmail.com>
2022-04-06 21:33:17 +0300

    Arttırma ve Eksiltme eklendi

---
+++ Operatörler/arttirma_eksiltme/arttirma_eksiltme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Arttirma_eksiltme
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        float f = 5.6f;
+
+        a++;
+
+        ++b;
+
+        c--;
+
+        --d;
+
+        f++;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(f);
+    }
+}
