d1e68a35a4d443a8ad38aaf2a17578057bf2e920
kadirdemir <kademir07@gmail.com>
2022-04-07 00:14:12 +0300

    Bitsel ÖZEL VEYA Operatörü eklendi

---
+++ Operatörler/bitsel_ozel_veya_operatoru/bitsel_ozel_veya_operatoru/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Bitsel_ozel_veya_operatoru
+{
+    static void Main()
+    {
+        byte b = 145;
+        byte c = 73;
+
+        Console.WriteLine("İlk Değer: " + c);
+
+        c = (byte)(c ^ b);
+        Console.WriteLine("İlk XOR: " + c);
+
+        c = (byte)(c ^ b);
+        Console.WriteLine("İkinci XOR: " + c);
+    }
+}
