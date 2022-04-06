de2fdb338cec5a0728aebb0360cad33c4c8ad80d
kadirdemir <kademir07@gmail.com>
2022-04-06 20:26:37 +0300

    Operatör Sırası eklendi

---
+++ Operatörler/operatör_sirasi/operatör_sirasi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Operatör_sirasi
+{
+    static void Main()
+    {
+        int i = 3 * 5 / 7;
+        Console.WriteLine(i);
+
+        int j = 3 * (5 / 7);
+        Console.WriteLine(j);
+
+        int k = 3 / 5 * 7;
+        Console.WriteLine(k);
+
+        int m = 5 / 7 * 3;
+        Console.WriteLine(m);
+    }
+}
