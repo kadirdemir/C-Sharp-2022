6d072743813978b531e2d8951e2fcf3a61fb7ad5
kadirdemir <kademir07@gmail.com>
2022-04-06 22:33:23 +0300

    Mantıksal VE Operatörü eklendi

---
+++ Operatörler/mantiksal_ve_operatörü/mantiksal_ve_operatörü/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Mantiksal_ve_operatoru
+{
+    static void Main()
+    {
+        bool b1 = 35 < 10 && 10 == 50; 
+        bool b2 = 35 > 10 && 10 != 50;
+        bool b3 = 14.6f > 10f && 0 != 100;
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+        Console.WriteLine(b3);
+        
+    }
+}
