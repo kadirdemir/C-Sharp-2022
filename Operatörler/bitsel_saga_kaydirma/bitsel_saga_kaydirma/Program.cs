a5f43edcf18555e113910447fcb5c3a1c1775d3d
kadirdemir <kademir07@gmail.com>
2022-04-07 00:36:39 +0300

    Bitsel Sağa Kaydırma eklendi

---
+++ Operatörler/bitsel_saga_kaydirma/bitsel_saga_kaydirma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Bitsel_saga_kaydirma
+{
+    static void Main()
+    {
+        byte b = 7;
+        byte c = (byte)(b >> 1);
+
+        Console.WriteLine("b: " + b);
+        Console.WriteLine("c: " + c);
+    }
+}
