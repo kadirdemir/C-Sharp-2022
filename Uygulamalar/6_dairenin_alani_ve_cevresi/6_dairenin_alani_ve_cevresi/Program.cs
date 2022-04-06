7c4cb05836fe0bd271499aa72730bb5868dbf079
kadirdemir <kademir07@gmail.com>
2022-04-06 15:17:05 +0300

    Dairenin Alanı ve Çevresi eklendi

---
+++ Uygulamalar/6_dairenin_alani_ve_cevresi/6_dairenin_alani_ve_cevresi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Dairenin_alani_ve_cevresi
+{
+    public static void Main()
+    {
+        double r, a, c;
+        const double pi = 3.14;
+        Console.Write("Yarıçap: ");
+        r = Convert.ToDouble(Console.ReadLine());
+        a = pi * r * r;
+        c = 2 * pi * r;
+        Console.Write("Alan= " + a + " Çevre: " + c);
+        Console.ReadLine();
+    }
+}
