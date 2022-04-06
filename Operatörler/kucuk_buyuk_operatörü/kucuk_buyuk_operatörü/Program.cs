6bbb2718cd49d9006e7fe1c28a17bbffac368ea4
kadirdemir <kademir07@gmail.com>
2022-04-06 21:52:29 +0300

    Küçük Büyük Operatörü eklendi

---
+++ Operatörler/kucuk_buyuk_operatörü/kucuk_buyuk_operatörü/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Kucuk_buyuk_operatörü
+{
+    static void Main()
+    {
+        bool b1 = 60 < 50;
+        bool b2 = 50 < 60;
+        bool b3 = 20.54 > 65.7;
+        bool b4 = 10.4f > 5.4;
+
+        Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", b1, b2, b3, b4);
+    }
+}
