db2cc5edf5225cea1affb38bba6c880254a27200
kadirdemir <kademir07@gmail.com>
2022-04-06 15:39:31 +0300

    Inch - cm dönüşümü eklendi

---
+++ Uygulamalar/7_inch_cm_donusumu/7_inch_cm_donusumu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Inch_cm_donusumu
+{
+    static void Main()
+    {
+        double i, cm;
+        const double oran = 2.54;
+
+        Console.Write("Inch değeri: ");
+        i = Convert.ToDouble(Console.ReadLine());
+
+        cm = oran * i;
+
+        Console.Write(i + " inch " + cm + " cm");
+
+        Console.ReadLine();
+    }
+}
