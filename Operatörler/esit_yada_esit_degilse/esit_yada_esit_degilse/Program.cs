256db3f602e631c88d597eb73c71185e591eb9ad
kadirdemir <kademir07@gmail.com>
2022-04-06 21:59:18 +0300

    Eşit ya da Eşit Değilse eklendi

---
+++ Operatörler/esit_yada_esit_degilse/esit_yada_esit_degilse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Esit_yada_esit_degilse
+
+{
+    static void Main()
+    {
+        bool b1 = 60 == 50;
+        bool b2 = 60 == 60;
+        bool b3 = 60 != 50;
+        bool b4 = 60 != 60;
+
+        Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", b1, b2, b3, b4);
+    }
+}
