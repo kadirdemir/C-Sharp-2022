a919dbe0fa56e1aaf85a361a8989e5971bb2de3f
kadirdemir <kademir07@gmail.com>
2022-04-06 15:43:52 +0300

    Fahrenhayt Santigrat Dönüşümü eklendi

---
+++ Uygulamalar/8_fahrenhayt_santigrat_donusumu/8_fahrenhayt_santigrat_donusumu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Fahrenhayt_santigrat_donusumu
+{
+    static void Main()
+    {
+        double c, f;
+        Console.Write("Fahrenhayt değeri: ");
+        f = Convert.ToDouble(Console.ReadLine());
+        c = (f - 32) / 1.8;
+        Console.Write("Fahrenhayt: " + f + " Santigrat: " + c);
+        Console.ReadLine();
+
+    }
+}
