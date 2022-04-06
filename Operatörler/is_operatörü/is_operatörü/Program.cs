72dc23ab7d66af566ed2a85297896c341acf261b
kadirdemir <kademir07@gmail.com>
2022-04-06 22:15:26 +0300

    is_Operatörü eklendi

---
+++ Operatörler/is_operatörü/is_operatörü/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Is_operatörü
+{
+    static void Main()
+    {
+        int i = 50;
+        bool b1 = i is int;
+        bool b2 = i is double;
+        bool b3 = i is float;
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+        Console.WriteLine(b3);
+    }
+}
