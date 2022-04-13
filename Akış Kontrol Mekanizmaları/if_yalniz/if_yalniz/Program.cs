cb4ef95da54790cea185869d82a3cbc0baa3df14
kadirdemir <kademir07@gmail.com>
2022-04-13 22:04:07 +0300

    if (else olmadan) eklendi

---
+++ Akış Kontrol Mekanizmaları/if_yalniz/if_yalniz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class If_yalniz
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 5;
+        //koşul doğru ise işlemler yapılır, yanlış ise if blogu dışına çıkılır
+        if (a<b)
+        {
+            a++;
+            Console.WriteLine("1. Deyim");
+        }
+    }
+}
