0aceb1559ad736aa36bc0b332991964aac3bdb3f
kadirdemir <kademir07@gmail.com>
2022-04-13 22:43:50 +0300

    switch 1 eklendi

---
+++ Akış Kontrol Mekanizmaları/switch_deyimi1/switch_deyimi1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Switch_deyimi1
+{
+    static void Main()
+    {
+        int a = 1;
+
+        switch (a)
+        {
+            case 1:
+                Console.WriteLine("Case1");
+                goto case 2;
+            case 2:
+                Console.WriteLine("Case2");
+                break;
+            default:
+                break;
+        }
+    }
+}
