49a8634a7b99b922f3139b5eaba1fffc08cd649e
kadirdemir <kademir07@gmail.com>
2022-04-13 23:41:57 +0300

    do while1 eklendi

---
+++ Akış Kontrol Mekanizmaları/do_while1/do_while1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Do_while1
+{
+    static void Main()
+    {
+        string yazi;
+        do
+        {
+            Console.WriteLine("Şifreyi Giriniz: ");
+            yazi = Console.ReadLine();
+
+        } while (yazi != "sifre");
+        Console.WriteLine("Şifreyi bildiniz");
+    }
+}
