f2852d36a23c03fb5925a23cdc9cde266e72b749
kadirdemir <kademir07@gmail.com>
2022-04-13 23:08:39 +0300

    is Desen Eşleştirme eklendi

---
+++ Akış Kontrol Mekanizmaları/is_desen_eslestirme/is_desen_eslestirme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Is_desen_eslestirme
+{
+    static void Main()
+    {
+        object bakiye = 100;
+        //sabit desen "null"
+
+        if (bakiye is null)
+        {
+            return;
+        }
+
+        //tur deseni "decimal kullaniciBakiyesi"
+        //decimal yerine int türü ile test edilse sonuç ekrana yazılabilirdi
+        if (bakiye is decimal kullaniciBakiyesi)
+        {
+            Console.WriteLine("Kullanıcının bakiyesi " + kullaniciBakiyesi);
+        }
+    }
+}
