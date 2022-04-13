964cd1b69bb79271ffee60fe16929ee0fb54583a
kadirdemir <kademir07@gmail.com>
2022-04-13 22:45:28 +0300

    switch 2 eklendi

---
+++ Akış Kontrol Mekanizmaları/switch_deyimi2/switch_deyimi2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Switch_deyimi2
+{
+    static void Main()
+    {
+        int a = 2;
+
+        switch (a)
+        {
+            case 1:
+            case 2:
+                Console.WriteLine("Case1 ve Case2");
+                break;
+            case 3:
+                Console.WriteLine("Case3");
+                break;
+            default:
+                break;
+        }
+    }
+}
