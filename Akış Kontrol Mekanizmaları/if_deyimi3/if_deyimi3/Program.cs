2b3244cfa581baf63a767773dbe79de99218b6b7
kadirdemir <kademir07@gmail.com>
2022-04-13 22:09:08 +0300

    if Deyimi 3 eklendi

---
+++ Akış Kontrol Mekanizmaları/if_deyimi3/if_deyimi3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class If_deyimi3
+{
+    static void Main()
+    {
+        int not;
+        Console.Write("Notunuzu giriniz (0-100): ");
+        not = Convert.ToInt32(Console.ReadLine());
+
+        if (not < 0 || not > 100)
+            Console.WriteLine("Yanlış not girdiniz.");
+        else if (not > 90)
+            Console.WriteLine("Notunuz: A");
+        else if (not > 80)
+            Console.WriteLine("Notunuz: B");
+        else if (not > 70)
+            Console.WriteLine("Notunuz: C");
+        else if (not > 50)
+            Console.WriteLine("Notunuz: D");
+        else
+            Console.WriteLine("Dersi Geçemediniz.");
+    }
+}
