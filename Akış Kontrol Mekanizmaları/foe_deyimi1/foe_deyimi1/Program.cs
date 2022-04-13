0c355597617380531052c1fc5191aeb064770381
kadirdemir <kademir07@gmail.com>
2022-04-13 23:30:42 +0300

    for Deyimi 1 eklendi

---
+++ Akış Kontrol Mekanizmaları/foe_deyimi1/foe_deyimi1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class For_deyimi1
+{
+    static void Main()
+    {
+        int toplam = 0;
+
+        for (int i = 1; i<=3; i++)
+        {
+            toplam = toplam + i;
+            Console.WriteLine("toplam={0}", toplam);
+        }
+    }
+}
