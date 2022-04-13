cc036503422bd8a700c4ac6cdf5e331a3e2fc709
kadirdemir <kademir07@gmail.com>
2022-04-13 23:37:05 +0300

    while Döngüsü 2 eklendi

---
+++ Akış Kontrol Mekanizmaları/while_dongusu2/while_dongusu2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class While_dongusu2
+{
+    static void Main()
+    {
+        int n = 0;
+        int toplam = 0;
+
+        while (n<=100)
+        {
+            toplam += n;
+            n += 2;
+        }
+        Console.WriteLine(toplam);
+    }
+}
