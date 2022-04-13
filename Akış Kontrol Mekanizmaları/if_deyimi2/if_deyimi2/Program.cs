a90c98a83b8752859227b3a44f826649a07e5e75
kadirdemir <kademir07@gmail.com>
2022-04-13 21:59:50 +0300

    if deyimi 2 eklendi

---
+++ Akış Kontrol Mekanizmaları/if_deyimi2/if_deyimi2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class If_deyimi2
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 7;
+        //Bir koşul altında birden fazla işlem yapmak için { } blok içine istenilen kadar deyim yazılabilir.
+        if (a<b)
+        {
+            a++;
+            Console.WriteLine("1. Deyim");
+            Console.WriteLine(a);
+        }
+        else
+        {
+            b--;
+            Console.WriteLine("2. Deyim");
+            Console.WriteLine(b);
+        }
+    }
+}
