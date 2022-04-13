fddf04c7597191aed0b0aca6a488cce3109ef394
kadirdemir <kademir07@gmail.com>
2022-04-13 23:34:36 +0300

    while Döngüsü 1 eklendi

---
+++ Akış Kontrol Mekanizmaları/while_dongusu1/while_dongusu1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class While_dongusu1
+{
+    static void Main()
+    {
+        int k = 1;
+        while (k <= 20)
+        {
+            Console.WriteLine(k);
+            k++;
+        }
+    }
+}
