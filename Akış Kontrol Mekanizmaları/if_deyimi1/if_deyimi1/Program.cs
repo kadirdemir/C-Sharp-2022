106382fb532243fdddb7fbcfe08fdcb26e36e6aa
kadirdemir <kademir07@gmail.com>
2022-04-13 21:52:32 +0300

    if deyimi 1 eklendi

---
+++ Akış Kontrol Mekanizmaları/if_deyimi1/if_deyimi1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class If_deyimi1
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 7;
+        //değer true ise 1. deyim, false ise 2. deyim çalıştırılır
+        if (a < b)
+            Console.WriteLine("1. Deyim");
+        else
+            Console.WriteLine("2. Deyim");
+    }
+}
