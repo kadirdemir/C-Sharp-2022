25a64aaffcf226b649dca58d1828a3d26a858c1e
kadirdemir <kademir07@gmail.com>
2022-04-14 06:57:46 +0300

    break ve continue eklendi

---
+++ Akış Kontrol Mekanizmaları/break_continue/break_continue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Break_continue
+{
+    static void Main()
+    {
+        int sayi;
+        int toplam = 0;
+        int bironcekisayi = 0;
+
+        //for (;;) ifadesi sonsuz döngü için kullanılır
+        for(;;)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi == 0)
+                break;
+
+            if (sayi == bironcekisayi)
+            {
+                Console.WriteLine("Farklı sayı giriniz: ");
+                continue;
+            }
+
+            toplam += sayi;
+            Console.WriteLine("Toplam ={0}", toplam);
+            bironcekisayi = sayi;
+        }
+        Console.WriteLine("Döngü bitti...");
+
+    }
+}
