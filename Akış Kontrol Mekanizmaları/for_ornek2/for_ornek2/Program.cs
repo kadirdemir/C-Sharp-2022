62890dcb534c881c875fe81d51933b1c8a4b807a
kadirdemir <kademir07@gmail.com>
2022-04-14 07:13:56 +0300

    for Örnek 2 eklendi

---
+++ Akış Kontrol Mekanizmaları/for_ornek2/for_ornek2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class For_ornek2
+{
+    static void Main()
+    {
+        int adet = 0;
+
+        for (int i=0; i<=100; i+=5)
+        {
+            if (adet %6 == 0 )
+                Console.WriteLine();
+            Console.Write("{0,5}", i);
+            adet++;
+        }
+        Console.WriteLine("\n");
+    }
+}
