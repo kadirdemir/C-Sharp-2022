d35c6f9f5b7d6c4a33ab634722559ac20855a0f1
kadirdemir <kademir07@gmail.com>
2022-04-14 07:22:54 +0300

    for Örnek 3 eklendi

---
+++ Akış Kontrol Mekanizmaları/for_ornek3/for_ornek3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class For_ornek3
+{
+    static void Main()
+    {
+        int satir, sutun;
+        Console.Write("Satır sayısını giriniz: ");
+        satir = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Sütun sayısını giriniz: ");
+        sutun = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        for (int i=0; i<sutun; i++)
+        {
+            for (int j = 0; j < satir; ++j)
+                Console.Write("*");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
