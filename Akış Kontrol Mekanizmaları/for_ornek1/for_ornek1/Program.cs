4e81233200e7eb7ea884ab38bd07cbad7814e29e
kadirdemir <kademir07@gmail.com>
2022-04-14 07:08:02 +0300

    for Örnek 1 eklendi

---
+++ Akış Kontrol Mekanizmaları/for_ornek1/for_ornek1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class For_ornek1
+{
+    static void Main()
+    {
+        int adet = 0, toplam = 0;
+        for (int i=1; i<=1000; i++)
+        {
+            if((i%5 ==0) && (i%7!=0))
+            {
+                Console.WriteLine(i);
+                toplam += i;
+                adet++;
+            }
+        }
+        Console.WriteLine("{0} sayı bulundu", adet);
+        Console.WriteLine("Toplamları= {0}", toplam);
+    }
+}
