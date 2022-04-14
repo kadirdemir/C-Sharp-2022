aecffa9f89bf954350341f43167c730167b98ded
kadirdemir <kademir07@gmail.com>
2022-04-14 07:33:24 +0300

    for Örnek 4 eklendi

---
+++ Akış Kontrol Mekanizmaları/for_ornek4/for_ornek4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class For_ornek4
+{
+    static void Main()
+    {
+        byte sayi;
+
+        Console.Write("Sayı giriniz (0-255): ");
+        sayi = Convert.ToByte(Console.ReadLine());
+
+        for (byte j = 8; j>=1; j--)
+        {
+            byte hane = 1;
+            for (int k = 1; k <= j - 1; k++)
+                hane *= 2;
+
+            byte bit = (byte)((sayi & hane) >> j - 1);
+            Console.Write(bit);
+        }
+
+    }
+}
