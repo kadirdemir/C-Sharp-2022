99f7169fd21f591066540c4932a764a40bba96bd
kadirdemir <kademir07@gmail.com>
2022-04-14 06:51:37 +0300

    break 1 Eklendi

---
+++ Akış Kontrol Mekanizmaları/break1/break1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Break1
+{
+    static void Main()
+    {
+        char ch;
+
+        do
+        {
+            Console.Write("Bir karakter giriniz: ");
+            ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'q' || ch == 'Q')
+                break;
+            else
+                Console.WriteLine("Döngüye devam...");
+        } while (true);
+
+        Console.WriteLine("Döngü dışı");
+    }
+}
