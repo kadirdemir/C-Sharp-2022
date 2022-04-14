dc15cd2906ea546c0d905b84dc5939bbffc8d417
kadirdemir <kademir07@gmail.com>
2022-04-14 08:08:32 +0300

    18 yaş kontrolü eklendi

---
+++ Uygulamalar/10_yas_kontrolu/10_yas_kontrolu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Yas_kontrolu
+{
+    static void Main()
+    {
+        int yas;
+
+        Console.Write("Yaşınızı giriniz: ");
+        yas = Convert.ToInt32(Console.ReadLine());
+
+        if (yas < 18)
+            Console.WriteLine("Yaşınız küçük");
+        else
+        Console.Write("Siteye girebilirsiniz");
+
+        Console.ReadLine();
+    }
+}
