99b0185508b17993cef43a1ed967b31b20ca3022
kadirdemir <kademir07@gmail.com>
2022-04-21 00:33:44 +0300

    1den n Değerine Kadar Çift Sayıları Yazdırma eklendi

---
+++ Uygulamalar/19_1den_n_degerine_kadar_cift_sayilar/19_1den_n_degerine_kadar_cift_sayilar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Birden_n_degerine_kadar_cift_sayilar
+{
+    static void Main()
+    {
+        int n, s = 0;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+    sayac_artirma:
+        s = s + 2;
+
+        Console.WriteLine(s);
+
+        if (s < n-1) goto sayac_artirma;
+
+        Console.ReadLine();
+
+    }
+}
