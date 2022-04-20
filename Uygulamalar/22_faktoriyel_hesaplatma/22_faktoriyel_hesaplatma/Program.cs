79f5eb1866df2d9a68936d8d00fa7950a713c5c7
kadirdemir <kademir07@gmail.com>
2022-04-21 00:55:12 +0300

    Faktöriyel Hesaplatma eklendi

---
+++ Uygulamalar/22_faktoriyel_hesaplatma/22_faktoriyel_hesaplatma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Faktoriyel_hesaplatma
+{
+    static void Main()
+    {
+        int s = 0, f = 1, n;
+
+        Console.Write("n:");
+        n =Convert.ToInt32(Console.ReadLine());
+
+    sayac_artirma:
+        s = s + 1;
+
+        if(s<=n)
+        {
+            f = f * s;
+            goto sayac_artirma;
+        }
+
+        Console.Write(n + "faktöriyel= " + f);
+
+        Console.ReadLine();
+    }
+}
