7318f24bffbb1e905d1d0352ede37a87f4bfa2c1
kadirdemir <kademir07@gmail.com>
2022-04-21 00:14:03 +0300

    İki Farklı Değer Kontrolü eklendi

---
+++ Uygulamalar/15_iki_farkli_deger_kontrolü/15_iki_farkli_deger_kontrolü/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Iki_farkli_deger_kontrolu
+{
+    static void Main()
+    {
+        int s1, s2;
+
+        Console.Write("Sayı 1: ");
+        s1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Sayı 2: ");
+        s2 = Convert.ToInt32(Console.ReadLine());
+
+        if (s1 == s2)
+            Console.Write("Sayı 1 = Sayı 2");
+        else if (s1 < s2)
+            Console.Write("Sayı 2 büyük");
+        else if (s1 > s2)
+            Console.Write("Sayı 1 büyük");
+
+        Console.ReadLine();
+    }
+}
