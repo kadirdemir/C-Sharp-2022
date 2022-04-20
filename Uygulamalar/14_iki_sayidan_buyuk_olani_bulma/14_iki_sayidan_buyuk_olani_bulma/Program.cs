d77998cccf4cd0438f5a3dc588d983289bb810c5
kadirdemir <kademir07@gmail.com>
2022-04-21 00:07:50 +0300

    İki Sayıdan Büyük Olanı Bulma eklendi

---
+++ Uygulamalar/14_iki_sayidan_buyuk_olani_bulma/14_iki_sayidan_buyuk_olani_bulma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class İki_sayidan_buyuk_olani_bulma
+{
+    static void Main()
+    {
+        int x, y;
+
+        Console.Write("x: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("y: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        if (x > y)
+            Console.Write("x büyük");
+        else
+            Console.Write("y büyük");
+
+        Console.ReadLine();
+    }
+}
