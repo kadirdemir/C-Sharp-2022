716c8f8610c569c903474c9591cf0bae0510950d
kadirdemir <kademir07@gmail.com>
2022-04-20 23:49:44 +0300

    Sadece Pozitif Sayıların Karesi eklendi

---
+++ Uygulamalar/11_sadece_pozitif_sayilarin_karesi/11_sadece_pozitif_sayilarin_karesi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Sadece_pozitif_sayi_kare
+{
+    static void Main()
+    {
+        int x, kare;
+
+    sayi_al:
+        Console.Write("Sayi: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        if (x < 0)
+            goto sayi_al;
+
+        kare = x * x;
+
+        Console.Write("Sayı: " + x + " Karesi: " + kare);
+
+        Console.ReadLine();
+    }
+                          
+}
