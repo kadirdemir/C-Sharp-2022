49edd17ca28998ce98161294f3bd586975330ae6
kadirdemir <kademir07@gmail.com>
2022-04-21 00:31:15 +0300

    1den n Değerine Kadar Yazdırma eklendi

---
+++ Uygulamalar/18_1den_n_degerine_kadar_yazdirma/18_1den_n_degerine_kadar_yazdirma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Birden_n_degerine_kadar_yazdirma
+{
+    static void Main()
+    {
+        int n, s = 0;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+    sayac_artirma:
+        s = s + 1;
+
+        Console.WriteLine(s);
+
+        if (s < n) goto sayac_artirma;
+
+        Console.ReadLine();
+    }
+}
