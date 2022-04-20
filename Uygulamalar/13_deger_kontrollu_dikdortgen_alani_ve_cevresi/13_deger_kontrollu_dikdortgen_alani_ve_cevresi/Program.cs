e2ccc3782402d787a9e3e4d26127850fe85908d5
kadirdemir <kademir07@gmail.com>
2022-04-21 00:03:11 +0300

    Değer Kontrollü Dikdörtgen Alanı ve Çevresi eklendi

---
+++ Uygulamalar/13_deger_kontrollu_dikdortgen_alani_ve_cevresi/13_deger_kontrollu_dikdortgen_alani_ve_cevresi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Deger_kontrollu_dikdortgen_alani_ve_cevresi
+{
+    static void Main()
+    {
+        double a, c, g, y;
+
+    g_oku:
+        Console.Write("Genişlik: ");
+        g = Convert.ToDouble(Console.ReadLine());
+        if (g <= 0) goto g_oku;
+
+        y_oku:
+        Console.Write("Yükseklik: ");
+        y = Convert.ToDouble(Console.ReadLine());
+        if (y <= 0) goto y_oku;
+
+        a = g * y;
+        c = 2 * (g + y);
+
+        Console.Write("Alan= " + a + " Çevre= " + c);
+
+        Console.ReadLine();
+    }
+}
