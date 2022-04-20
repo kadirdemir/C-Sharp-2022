3d203d4186b9e593238bd963459c784a6acef7db
kadirdemir <kademir07@gmail.com>
2022-04-21 00:41:49 +0300

    Ekrandan Belli Adet Sayı Alma eklendi

---
+++ Uygulamalar/20_ekrandan_belli_adet_sayi_alma/20_ekrandan_belli_adet_sayi_alma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Ekrandan_belli_adet_sayi_alma
+{
+    static void Main()
+    {
+        int adet, sayi, top = 0, sayac = 0;
+
+        Console.Write("Adet: ");
+        adet =Convert.ToInt32(Console.ReadLine());
+
+    oku:
+        Console.Write("Sayı Gir: ");
+        sayi =Convert.ToInt32(Console.ReadLine());
+
+        sayac = sayac + 1;
+
+        top = top + sayi;
+
+        if (sayac < adet)
+            goto oku;
+        else
+            Console.WriteLine("toplam" + top);
+        Console.ReadLine();
+    }
+}
