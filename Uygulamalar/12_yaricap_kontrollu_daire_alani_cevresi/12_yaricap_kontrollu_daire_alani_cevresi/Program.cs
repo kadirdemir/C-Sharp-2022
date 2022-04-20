b97e5c8f761dea5a9486e9b4135b54b778127418
kadirdemir <kademir07@gmail.com>
2022-04-20 23:58:45 +0300

    Yarıçap Kontrollü Daire Çevresi ve Alanı eklendi

---
+++ Uygulamalar/12_yaricap_kontrollu_daire_alani_cevresi/12_yaricap_kontrollu_daire_alani_cevresi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Yaricap_kontrollu_daire_alani_cevresi
+{
+    static void Main()
+    {
+        double r, a, c;
+        const double pi = 3.1415;
+
+    r_al:
+        Console.Write("Yarıçap: ");
+        r= Convert.ToDouble(Console.ReadLine());
+
+        if (r <= 0)
+            goto r_al;
+
+        a = pi * r * r;
+        c = 2 * pi * r;
+
+        Console.Write("Alan: " + a + "Çevre: " + c);
+
+        Console.ReadLine();
+    }
+}
