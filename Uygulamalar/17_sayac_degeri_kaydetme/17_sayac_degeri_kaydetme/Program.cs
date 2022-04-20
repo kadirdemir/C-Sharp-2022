61b738ed1282e703c10333bd4c30ced1778b8324
kadirdemir <kademir07@gmail.com>
2022-04-21 00:27:23 +0300

    Sayaç Değeri Kaydetme eklendi

---
+++ Uygulamalar/17_sayac_degeri_kaydetme/17_sayac_degeri_kaydetme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Sayac_degeri_kaydetme
+{
+    static void Main()
+    {
+        int sayi, sayac = 0;
+
+    oku_sayi:
+        Console.Write("Sayı giriniz: ");
+        sayi = Convert.ToInt32(Console.ReadLine());
+
+        sayac = sayac + 1;
+
+        if (sayi == 5)
+            Console.Write("Sayaç= " + sayac);
+        else
+            goto oku_sayi;
+
+            Console.ReadLine();
+    }
+}
