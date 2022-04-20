dc7127a77c69ff2bf3f24cca534b37e1f340c5e2
kadirdemir <kademir07@gmail.com>
2022-04-21 00:51:33 +0300

    1den n Değerine Kadar Sayıların Toplamı eklendi

---
+++ Uygulamalar/21_1den_n_degerine_kadar_sayilarin_toplami/21_1den_n_degerine_kadar_sayilarin_toplami/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Birden_n_degerine_kadar_sayilarin_toplami
+{
+    static void Main()
+    {
+        int top=0, s=0, n;
+
+        Console.Write("n:");
+        n = Convert.ToInt32(Console.ReadLine());
+
+    sayac_artirma:
+        s = s + 1;
+        top = top + s;
+
+        if (s < n)
+            goto sayac_artirma;
+        else
+            Console.WriteLine("Toplam= " + top);
+        Console.ReadLine();
+    }
+}
