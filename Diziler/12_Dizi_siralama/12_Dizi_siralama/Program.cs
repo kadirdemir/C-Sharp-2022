25fee514ccdecbf75e02f3ba13450c029f8a671f
kadirdemir <kademir07@gmail.com>
2022-05-26 00:53:41 +0300

    Dizi sıralama eklendi

---
+++ Diziler/12_Dizi_siralama/12_Dizi_siralama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Dizi_siralama
+{
+    static void Main()
+    {
+        Array strArray = Array.CreateInstance(typeof(string), 6);
+        strArray.SetValue ("Sefer", 0);
+        strArray.SetValue ("Mehmet", 1);
+        strArray.SetValue ("Tahir", 2);
+        strArray.SetValue ("Yusuf", 3);
+        strArray.SetValue ("Yunus", 4);
+        strArray.SetValue ("Gökçen", 5);
+
+        Console.WriteLine("Sırasız Dizi:\n");
+        foreach(string isim in strArray)
+            Console.Write(isim + "-");
+
+        Console.WriteLine();
+
+        Console.WriteLine("\nSıralı Dizi: \n");
+            Array.Sort(strArray);
+
+        foreach (string isim in strArray)
+            Console.Write(isim + "-");
+
+        Console.ReadLine();
+
+    }
+}
