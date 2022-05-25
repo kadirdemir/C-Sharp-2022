40512f8c0a590e3c87cd66b34548a95e6ba4f403
kadirdemir <kademir07@gmail.com>
2022-05-25 22:31:46 +0300

    İki değer arası toplama eklendi

---
+++ Uygulamalar/37_iki_deger_arasi_toplam/37_iki_deger_arasi_toplam/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Iki_deger_arasi_toplam
+{
+    static void Main()
+    {
+        int x, y, top=0;
+
+        Console.Write("x değeri: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("y değeri: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        for (; x <= y; x++)
+            top += x;
+
+        Console.Write("toplam= " + top);
+        Console.ReadLine();
+    }
+}
