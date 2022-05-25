54efb3d73757faa086d3986604936b9092202b69
kadirdemir <kademir07@gmail.com>
2022-05-25 21:20:55 +0300

    Telefon Konuşma Ücreti eklendi

---
+++ Uygulamalar/29_telefon_konusma_ucreti/29_telefon_konusma_ucreti/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Telefon_konusma_ucreti
+{
+    static void Main()
+    {
+        double sure, ucret;
+
+        Console.Write("Telefon konuşma süresi (dk): ");
+        sure = Convert.ToDouble(Console.ReadLine());
+
+        if (sure <= 3.0)
+            ucret = 0.25;
+        else
+            ucret = 0.25 + ((sure - 3) * 0.08);
+
+        Console.Write(sure + " dk telefon konuşma ücreti: " + ucret + " Türk Lirası");
+        Console.ReadLine();
+
+    }
+}
