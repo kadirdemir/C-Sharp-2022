7b3f9a74744502afdbe74dd6fdd93803780326c7
kadirdemir <kademir07@gmail.com>
2022-05-26 00:18:16 +0300

    CreateInstance üç boyutlu dizi oluşturma eklendi

---
+++ Diziler/10_Create_instance__uc_boyutlu_dizi_tanimlama/10_Create_instance__uc_boyutlu_dizi_tanimlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Create_instance__uc_boyutlu_dizi_tanimlama
+{
+    static void Main()
+    {
+        Array dizi = Array.CreateInstance(typeof(int), 5,4,3);
+
+        for (int i = 0; i <= dizi.GetUpperBound(0); i++)
+            for(int j = 0; j <= dizi.GetUpperBound(1); j++)
+                for(int k = 0; k <= dizi.GetUpperBound(2); k++)
+                    dizi.SetValue(i+j+k, i, j, k);
+
+        for (int i = 0; i <= dizi.GetUpperBound(0); i++)
+            for (int j = 0; j <= dizi.GetUpperBound(1); j++)
+                for (int k = 0; k <= dizi.GetUpperBound(2); k++)
+                    Console.WriteLine(dizi.GetValue(i,j,k));
+    }
+}
