62adee01f3f8dcc50762c588092b739ba8866fe3
kadirdemir <kademir07@gmail.com>
2022-05-25 23:20:23 +0300

    Çalışma zamanında dizi boyutu belirleme eklendi

---
+++ Diziler/3_calisma_zamaninda_dizi_boyutu_belirleme/3_calisma_zamaninda_dizi_boyutu_belirleme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Calisma_zamaninda_dizi_boyutu_belirleme
+{
+    static void Main()
+    {
+        Console.Write("Dinamik dizinin boyutu: ");
+        int val = Convert.ToInt32(Console.ReadLine());
+
+        int [] dizi = new int [val];
+
+        for (int i = 0; i < val; i++)
+            Console.WriteLine(dizi[i]);
+    }
+}
