56aeda5d55731f6cff211f1c545d657038bc2df8
kadirdemir <kademir07@gmail.com>
2022-05-25 22:25:59 +0300

    faktöriyel for ve while eklendi

---
+++ Uygulamalar/35_faktoriyel_for_while/35_faktoriyel_for_while/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Faktoriyel_for_while
+{
+    static void Main()
+    {
+        int n, f = 1;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int s = 1; s <= n; s++)
+            f*=s;
+        Console.WriteLine("for ile " + n + "!= " +f);
+
+        int say = 1;
+        f = 1;
+
+        while (say <= n)
+        {
+            f *= say;
+            say++;
+        }
+        Console.WriteLine("while ile " + n + "!= " + f);
+
+        Console.ReadLine();
+
+
+    }
+}
+
+
