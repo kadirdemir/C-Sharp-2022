fe2d37d1a468449a7177972f07cdafc0e97b6d4c
kadirdemir <kademir07@gmail.com>
2022-05-25 23:17:07 +0300

    Dizinin elemanlarını tanımlama eklendi

---
+++ Diziler/2_dizi_elemanlarini_tanimlama/2_dizi_elemanlarini_tanimlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Dizinin_elemanlarini_tanimlama
+{
+    static void Main()
+    {
+        string[] dizi1 = {"Bir", "İki", "Üç"};
+        int[] dizi2 = {11,22,33};
+        float[] dizi3 = {1f, 2f, 3f};
+
+        for (int i = 0; i < dizi1.Length; ++i)
+        {
+            Console.Write("{0,5}", dizi1[i]);
+            Console.Write("{0,6}", dizi2[i]);
+            Console.WriteLine("{0,5}", dizi3[i]);
+        }
+    }
+}
