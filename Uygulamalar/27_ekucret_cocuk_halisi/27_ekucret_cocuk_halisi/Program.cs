d1bf5bb3cc7cbf8b7045bae02d85df4535482ea2
kadirdemir <kademir07@gmail.com>
2022-05-25 21:20:16 +0300

    Ek Ücret Çocuk Halısı eklendi

---
+++ Uygulamalar/27_ekucret_cocuk_halisi/27_ekucret_cocuk_halisi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Ek_ucret_cocuk_halisi
+{
+    static void Main()
+    {
+        string kod;
+        double alan, ucret = 0;
+
+        Console.Write("Halı Tipi seçiniz (T/B/S): ");
+        kod = Convert.ToString(Console.ReadLine());
+
+        Console.Write("Kaç metre halı döşenecek: ");
+        alan = Convert.ToDouble(Console.ReadLine());
+
+        if (kod == "T")
+            ucret = alan * 18.8;
+        else if (kod == "B")
+            ucret = alan * 17.8;
+        else if (kod == "S")
+            ucret = alan * 19.2;
+
+        if (alan < 25)
+            ucret += 35;
+        else if (alan < 100)
+            ucret += 60;
+        else ucret += 75;
+
+
+        Console.Write("Ödenmesi gereken ücret: " + ucret);
+
+        Console.ReadLine();
+
+    }
+}
