b06c3e80f2eb0c0b7c6859f3ffe2c9e72dde32a8
kadirdemir <kademir07@gmail.com>
2022-05-25 21:20:36 +0300

    Mil - Km Çevirici eklendi

---
+++ Uygulamalar/28_mil_km_cevirici/28_mil_km_cevirici/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Mil_km_cevirici
+{
+    static void Main()
+    {
+        const float oran = 1.609344F;
+
+        string sec;
+
+        double mesafe, sonuc;
+
+        Console.Write("Mil (M/m) veya Kilometre (K/k) seçiniz: ");
+        sec = Convert.ToString(Console.ReadLine());
+
+        if ((sec == "M") || (sec == "m"))
+        {
+            Console.Write("Kaç mil yol aldınız: ");
+            mesafe = Convert.ToDouble(Console.ReadLine());
+            sonuc = mesafe * oran;
+            Console.Write(mesafe + " Mil = " + sonuc + " Kilometre");
+            Console.ReadLine();
+        }
+        else if ((sec == "K") || (sec == "k"))
+        {
+            Console.Write("Kaç km yol aldınız: ");
+            mesafe = Convert.ToDouble(Console.ReadLine());
+            sonuc = mesafe / oran;
+            Console.Write(mesafe + " Kilometre = " + sonuc + " Mil");
+            Console.ReadLine();
+        }
+    }
+}
