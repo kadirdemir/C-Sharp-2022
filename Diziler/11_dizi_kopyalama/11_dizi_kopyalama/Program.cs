1a9ad87982a94bb848e88dc48a31d69f71a8e73e
kadirdemir <kademir07@gmail.com>
2022-05-26 00:23:31 +0300

    Dizi kopyalama eklendi

---
+++ Diziler/11_dizi_kopyalama/11_dizi_kopyalama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Dizi_kopyalama
+{
+    static void Main()
+    {
+        int [] dizi1 = {1, 2, 3, 4, 5, 6, 7};
+        int[] dizi2 = new int[10];
+
+        //dizi1 dizi2'ye 5. indeksten 4 adet veri kopyalanıyor
+        Array.Copy(dizi1, 2, dizi2, 5, 4);
+
+        foreach(int i in dizi2)
+            Console.Write(i);
+    }
+}
