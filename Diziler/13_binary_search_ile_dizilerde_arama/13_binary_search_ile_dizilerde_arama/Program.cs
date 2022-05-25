b57732870b77f54d3ff2982cb26da4c6a58704c5
kadirdemir <kademir07@gmail.com>
2022-05-26 00:58:42 +0300

    Binary search ile dizilerde arama eklendi

---
+++ Diziler/13_binary_search_ile_dizilerde_arama/13_binary_search_ile_dizilerde_arama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Binary_search_ile_dizilerde_arama
+{
+    static void Main()
+    {
+        string[] dizi = new string[5];
+        string str;
+        int indeks;
+
+        dizi.SetValue("Sefer", 0);
+        dizi.SetValue("Feride", 1);
+        dizi.SetValue("Gökçen", 2);
+        dizi.SetValue("İhsan", 3);
+        dizi.SetValue("Yusuf", 4);
+
+        Console.WriteLine("Aranacak Yazı: ");
+
+        str = Console.ReadLine();
+
+        Array.Sort(dizi);
+
+        indeks =Array.BinarySearch(dizi,str);
+
+        if (indeks <= -1)
+            Console.WriteLine("Yazı bulunamadı...");
+        else
+            Console.WriteLine("{0}. indekste bulundu", indeks);
+
+    }
+}
