2d003a22d9296a11a25c08eb5625d1c1a832da16
kadirdemir <kademir07@gmail.com>
2022-05-25 23:47:57 +0300

    Matris dizileri oluşturma ve ekrana yazdırma eklendi

---
+++ Diziler/6_matris_diziler/6_matris_diziler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Matris_diziler
+{
+    static void Main()
+    {
+        //3*2 boyutlu matris dizisi oluşturma
+        int[,] dizi = { { 1,2 }, { 3, 4 }, { 5, 6 } };
+
+        //İç içe for döngüsü ile matris dizisini ekrana yazdırma
+        for(int i = 0; i < dizi.GetLength(0); i++)
+            for(int j = 0; j < dizi.GetLength(1); j++)
+                Console.WriteLine("dizi[" + i + "," + j + "] = " + dizi[i,j]);
+    }
+}
