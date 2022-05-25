7b323e5e04cd253e9302948bc92c391a414b6e3a
kadirdemir <kademir07@gmail.com>
2022-05-25 22:16:30 +0300

    1'den n'e kadar olan sayıların toplamı (for) eklendi

---
+++ Uygulamalar/33_1_den_n_e_kadar_olan_sayilar_toplam_for/33_1_den_n_e_kadar_olan_sayilar_toplam_for/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Birden_n_e_kadar_olan_sayilar_toplam_for
+{
+    static void Main()
+    {
+        int s, n, top=0;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (s = 1; s <= n; s++)
+            top += s;
+        Console.WriteLine("for ile toplam: " + top);
+
+
+        top = 0;
+        s = 1;
+        while (s <= n)
+        {
+            top += s;
+            s++;
+        }
+        Console.WriteLine("while ile toplam: " + top);
+
+        Console.ReadLine();
+
+
+    }
+}
+
