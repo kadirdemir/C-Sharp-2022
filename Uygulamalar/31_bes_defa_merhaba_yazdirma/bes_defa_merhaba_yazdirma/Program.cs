ac7c302c62618e62c8a76068a76f65aab7f1f070
kadirdemir <kademir07@gmail.com>
2022-05-25 21:55:30 +0300

    Beş Defa Merhaba Yazdırma (for) eklendi

---
+++ Uygulamalar/31_bes_defa_merhaba_yazdirma/bes_defa_merhaba_yazdirma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Bes_defa_merhaba_yazdirma
+{
+    static void Main()
+    {
+        int s = 1;
+    yaz:
+        Console.WriteLine("Merhaba (if)");
+        s = s + 1;
+        if (s <= 5) goto yaz;
+
+        Console.WriteLine();
+
+        for (s = 1; s <= 5; s++)
+            Console.WriteLine("Merhaba (for)");
+        Console.WriteLine();
+
+        s = 1;
+        while(s <= 5)
+        {
+            Console.WriteLine("Merhaba (while)");
+            s++;
+        }
+
+        Console.ReadLine();
+
+
+    }
+}
