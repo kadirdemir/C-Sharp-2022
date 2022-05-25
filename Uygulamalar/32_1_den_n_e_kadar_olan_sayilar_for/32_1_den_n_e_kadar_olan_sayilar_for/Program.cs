257546ee3d388567cb3aaf78c67eb6604febcfec
kadirdemir <kademir07@gmail.com>
2022-05-25 22:03:03 +0300

    1'den n'e kadar olan sayılar (for) eklendi

---
+++ Uygulamalar/32_1_den_n_e_kadar_olan_sayilar_for/32_1_den_n_e_kadar_olan_sayilar_for/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Birden_n_e_kadar_olan_sayilar_for
+{
+    static void Main()
+    {
+        int s, n;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (s = 1; s <= n; s++)
+            Console.WriteLine(s);
+
+
+        Console.WriteLine();
+
+        s = 1;
+        while (s <= n)
+        {
+            Console.WriteLine(s);
+            s++;
+        }
+
+        Console.ReadLine();
+
+
+    }
+}
