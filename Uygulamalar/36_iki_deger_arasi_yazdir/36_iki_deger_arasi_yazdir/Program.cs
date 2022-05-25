4ab9e171588bd370b888de013231a96ceec5cb93
kadirdemir <kademir07@gmail.com>
2022-05-25 22:29:48 +0300

    İki değer arası ekrana yazdırma eklendi

---
+++ Uygulamalar/36_iki_deger_arasi_yazdir/36_iki_deger_arasi_yazdir/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Iki_deger_arasi_yazdir
+{
+    static void Main()
+    {
+        int x, y;
+
+        Console.Write("x değeri: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("y değeri: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        for (; x <= y; x++)
+            Console.WriteLine(x);
+
+        Console.ReadLine();
+    }
+}
