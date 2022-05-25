5aa98d519c6d291beea41677de9f37c4ff7556e0
kadirdemir <kademir07@gmail.com>
2022-05-25 21:17:39 +0300

    Halı Döşeme eklendi

---
+++ Uygulamalar/24_hali_doseme/24_hali_doseme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Hali_doseme
+{
+    static void Main()
+    {
+        int metrekare, ucret, doseme=100, birimfiyat=35;
+
+        Console.Write("Metrekare giriniz: ");
+        metrekare = Convert.ToInt32(Console.ReadLine());
+
+        ucret = metrekare * birimfiyat + doseme;
+
+        Console.Write("Ucret: " + ucret);
+
+        Console.ReadLine();
+
+    }
+}
