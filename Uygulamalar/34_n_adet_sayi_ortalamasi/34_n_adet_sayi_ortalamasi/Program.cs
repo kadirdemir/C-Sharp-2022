a831f033838f5860c519f5d6ba3de4a248d23c9f
kadirdemir <kademir07@gmail.com>
2022-05-25 22:20:43 +0300

    n adet sayı ortalaması eklendi

---
+++ Uygulamalar/34_n_adet_sayi_ortalamasi/34_n_adet_sayi_ortalamasi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class N_adet_sayi_ortalamasi
+{
+    static void Main()
+    {
+        int s, n, sayi, top = 0;
+
+        Console.Write("n değeri: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (s = 1; s <= n; s++)
+        {
+            Console.Write(s + ". sayı:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            top += sayi;
+        }
+        Console.WriteLine("for ile ortalama: " + top/n);
+
+
+        top = 0;
+        s = 1;
+        while (s <= n)
+        {
+            Console.Write(s + ". sayı:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            top += sayi;
+            s++;
+        }
+        Console.WriteLine("while ile ortalama: " + top / n);
+
+        Console.ReadLine();
+
+
+    }
+}
+
+
