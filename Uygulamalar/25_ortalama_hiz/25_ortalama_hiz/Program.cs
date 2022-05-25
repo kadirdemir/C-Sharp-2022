cff95291a842cec699b201e94b1a970133c72609
kadirdemir <kademir07@gmail.com>
2022-05-25 21:18:35 +0300

    Ortalama Hız eklendi

---
+++ Uygulamalar/25_ortalama_hiz/25_ortalama_hiz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Hiz_donusumu
+{
+    static void Main()
+    {
+        const int uzaklik = 453;
+        double saat, hiz;
+
+        Console.Write("Kaç saatte gittiğinizi giriniz: ");
+        saat = Convert.ToDouble(Console.ReadLine());
+
+        hiz = uzaklik / saat;
+
+        Console.Write("Arabanın ortalama hızı: " + hiz);
+
+        Console.ReadLine();
+
+
+    }
+}
