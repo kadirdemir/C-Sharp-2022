d41390bba2059c74492bac320f437aaabfda6742
kadirdemir <kademir07@gmail.com>
2022-05-25 22:37:49 +0300

    Tek bilinmeyenli denklem içiçe for eklendi

---
+++ Uygulamalar/39_tek_bilinmeyenli_denklem_icice_for/39_tek_bilinmeyenli_denklem_icice_for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Tek_bilinmeyenli_denklem_icice_for
+{
+    static void Main()
+    {
+        double x, a, b;
+
+        for(b=1; b<=5; b++)
+        {
+            for(a=1; a<=5; a++)
+            {
+                x = -b / a;
+                Console.WriteLine("b=" + b + "\t a= " + a + "\t x= " + x);
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
