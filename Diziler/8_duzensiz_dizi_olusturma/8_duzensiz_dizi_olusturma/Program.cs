acd34e4a0d2a47cd267d13f22511214660c34cb2
kadirdemir <kademir07@gmail.com>
2022-05-26 00:04:23 +0300

    Düzensiz dizi oluşturma eklendi

---
+++ Diziler/8_duzensiz_dizi_olusturma/8_duzensiz_dizi_olusturma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Duzensiz_dizi_olusturma
+{
+    static void Main()
+    {
+        int[][] dizi = new int[3][];
+
+        dizi[0] = new int[] { 1, 2, 3 };
+        dizi[1] = new int[] { 4, 5, 6, 7 };
+        dizi[2] = new int[] { 8, 9};
+
+        for (int i = 0; i < dizi.GetLength(0); ++i)
+            for (int j = 0; j < dizi[i].GetLength(0); ++j)
+                Console.WriteLine("dizi[{0}][{1}]={2}", i, j, dizi[i][j]);
+    }
+}
