132da8bc631f2703258b3b74d4f5f6b6461f86b4
kadirdemir <kademir07@gmail.com>
2022-05-25 21:21:16 +0300

    Bilye Hesaplama eklendi

---
+++ Uygulamalar/30_Bilye_hesaplama/30_Bilye_hesaplama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Bilye_hesaplama
+{
+    static void Main()
+    {
+
+
+        for (int bilye=1; bilye<=1000; bilye++)
+            if (bilye%2==1 && bilye%3==1 && bilye%4==1 && bilye%5==1 && bilye%6==1 && bilye%7==0)
+            {
+                Console.Write("Bilye sayısı: " +bilye + "\n");
+                break;
+
+            }
+    }
+}
