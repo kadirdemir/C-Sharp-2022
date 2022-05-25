398eca393d6e71ecd3aeea5b4b9d3fd210703d06
kadirdemir <kademir07@gmail.com>
2022-05-26 00:12:02 +0300

    CreateInstance ile dizi tanımlama ve ekrana yazdırma eklendi

---
+++ Diziler/9_create_instance_dizi_tanimlama/9_create_instance_dizi_tanimlama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Create_instance_dizi_tanimlama
+{
+    static void Main()
+    {
+        //5 elemanlı bir dizi oluşturuluyor
+        Array dizi = Array.CreateInstance(typeof(int), 5);
+        foreach(int i in dizi)
+            Console.WriteLine(i);
+
+
+        //Üç boyutlu dizi oluşturma
+        //CreateInstance(Type, int, int, int);
+
+        //Type türünden int ile verilen sayı kadar elemanı olan dizi tanımlama
+        //CreateInstance(Type, int);
+
+        //Çok boyutlu diziler oluşturma
+        //CreateInstance(Type, int []);
+
+        //İki boyutlu dizi oluşturma
+        //CreateInstance(Type, int, int);
+
+
+
+    }
+}
