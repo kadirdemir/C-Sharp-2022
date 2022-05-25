e4933ca950270b95b5753731b6574f6cd047f58d
kadirdemir <kademir07@gmail.com>
2022-05-25 21:20:04 +0300

    Çocuk Halısı eklendi

---
+++ Uygulamalar/26_cocukhalisi/26_cocukhalisi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Cocuk_halisi
+{
+    static void Main()
+    {
+        string kod;
+        double alan, ucret = 0;
+
+        Console.Write("Halı Tipi seçiniz (T/B/S): ");
+        kod = Convert.ToString(Console.ReadLine());
+
+        Console.Write("Kaç metre halı döşenecek: ");
+        alan = Convert.ToDouble(Console.ReadLine());
+
+        if (kod == "T")
+            ucret = alan * 18.8;
+        else if (kod == "B")
+            ucret = alan * 17.8;
+        else if (kod == "S")
+            ucret = alan * 19.2;
+
+        Console.Write("Ödenmesi gereken ücret: " + ucret);
+
+        Console.ReadLine();
+
+    }
+}
