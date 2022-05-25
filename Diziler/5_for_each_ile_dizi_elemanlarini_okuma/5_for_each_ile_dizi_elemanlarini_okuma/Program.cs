fde197918c91253633dc35dd5d676e830592c06f
kadirdemir <kademir07@gmail.com>
2022-05-25 23:38:46 +0300

    Foreach ile dizi elemanlarını okuma eklendi

---
+++ Diziler/5_for_each_ile_dizi_elemanlarini_okuma/5_for_each_ile_dizi_elemanlarini_okuma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class For_each_ile_dizi_elemanlarini_okuma
+{
+    static void Main()
+    {
+        int [] dizi = { 1, 2, 3, 4, 5};
+        //foreach döngüsü ile bir dizinin elemanlarına sadece okumak için erişilir
+        //foreach ile dizi elemanı değiştirmeye çalışılırsa hata ile karşılaşılır
+        foreach (int eleman in dizi) 
+            Console.WriteLine(eleman);
+    }
+}
