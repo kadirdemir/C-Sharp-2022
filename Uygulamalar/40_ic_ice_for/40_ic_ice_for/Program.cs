4e539faaba60868de334798230b7c4322ab4b497
kadirdemir <kademir07@gmail.com>
2022-05-25 22:43:12 +0300

    İç içe for eklendi

---
+++ Uygulamalar/40_ic_ice_for/40_ic_ice_for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Icice_for
+{
+    static void Main()
+    {
+        double a, b;
+
+        for (a = 1; a <= 2; a++)
+        {
+            Console.WriteLine("for a= " + a);
+
+            for (b = 1; b <= 2; b++)
+            {
+                Console.WriteLine("for b= " + b);
+
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
