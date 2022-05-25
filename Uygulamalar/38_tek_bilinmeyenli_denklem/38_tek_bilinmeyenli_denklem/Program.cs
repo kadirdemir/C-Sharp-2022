7af95717f5e4ca5bfe2218db8719d29cd107bb1e
kadirdemir <kademir07@gmail.com>
2022-05-25 22:34:39 +0300

    Tek bilinmeyenli denklem eklendi

---
+++ Uygulamalar/38_tek_bilinmeyenli_denklem/38_tek_bilinmeyenli_denklem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Tek_bilinmeyenli_denklem
+{
+    static void Main()
+    {
+        double x, a, b = 1;
+
+        for(a=1; a<=10; a++)
+        {
+            x = -b / a;
+            Console.WriteLine("a= " + a + "\t x= " + x);
+
+        }
+        Console.ReadLine();
+    }
+}
