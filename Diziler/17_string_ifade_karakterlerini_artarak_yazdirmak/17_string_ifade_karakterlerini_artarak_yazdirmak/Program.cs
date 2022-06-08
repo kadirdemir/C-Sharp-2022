41da292c7fe729eabd41427c2266eefcbdf5eef9
kadirdemir <kademir07@gmail.com>
2022-06-08 23:50:38 +0300

    String ifade karakterlerini alt alta yazdırmak eklendi

---
+++ Diziler/17_string_ifade_karakterlerini_artarak_yazdirmak/17_string_ifade_karakterlerini_artarak_yazdirmak/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Metin giriniz: ");
+string metin =Console.ReadLine();
+
+metin = metin.ToUpper();
+
+for (int i = 0; i <= metin.Length; i++)
+    Console.WriteLine(metin.Substring(0,i));
+
+Console.ReadLine();
