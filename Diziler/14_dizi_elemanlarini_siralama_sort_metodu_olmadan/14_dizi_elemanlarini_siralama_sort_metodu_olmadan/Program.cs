b4def4db913025c21a610c555f2f89c78a8df662
kadirdemir <kademir07@gmail.com>
2022-06-08 23:49:37 +0300

    Dizi elemanlarını sıralama - sort metodu olmadan - eklendi

---
+++ Diziler/14_dizi_elemanlarini_siralama_sort_metodu_olmadan/14_dizi_elemanlarini_siralama_sort_metodu_olmadan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Dizi_elemanlarini_siralama_sort_metodu_olmadan
+{
+    static void Main()
+    {
+        int[] dizi = { 5, 2, 1, 4, 6, 0, 7 };
+        int gecici, j;
+
+        for (int i = 1; i < dizi.Length; ++i)
+        {
+            if (dizi[i] < dizi[i - 1])
+            {
+                gecici = dizi[i];
+                for (j = i; j > 0 && gecici < dizi[j - 1]; --j)
+                    dizi[j] = dizi[j - 1];
+                dizi[j] = gecici;
+            }
+        }
+        foreach (int i in dizi)
+            Console.WriteLine(i);
+    }
+}
