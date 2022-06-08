dd3145e38b504a06ee1009a974d5d0312790f140
kadirdemir <kademir07@gmail.com>
2022-06-08 23:50:10 +0300

    Diziye ait ortalama min - mak değer bulma eklendi

---
+++ Diziler/16_diziye_ait_ortalama_min_mak_deger_bulma/16_diziye_ait_ortalama_min_mak_deger_bulma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Diziye_ait_ortalama_min_mak_deger_bulma
+{
+    static void Main()
+    {
+        int[] dizi = new int[20];
+        Random rnd = new();
+
+        int max = 0, min = 100, toplam = 0;
+
+        for(int i = 0; i < dizi.Length; ++i)
+        {
+            dizi[i] = rnd.Next(0,100);
+
+            if(dizi[i] > max)
+                max = dizi[i];
+            if(dizi[i] < min)
+                min = dizi[i];
+            toplam += dizi[i];
+        }
+
+        foreach (int i in dizi)
+            Console.Write(i+" ");
+
+        Console.WriteLine("\nEn büyük: {0}", max);
+        Console.WriteLine("En küçük: {0}", min);
+
+        Console.WriteLine("Ortalama: {0}", (float)toplam/(float)dizi.Length);
+    }
+}
