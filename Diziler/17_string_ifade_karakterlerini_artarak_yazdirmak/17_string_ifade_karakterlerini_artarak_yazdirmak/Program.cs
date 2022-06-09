ac46d19cd5f3baa48c24e9ce31348e779eb7bc3d
kadirdemir <kademir07@gmail.com>
2022-06-09 09:39:07 +0300

    silindi

--- Diziler/17_string_ifade_karakterlerini_artarak_yazdirmak/17_string_ifade_karakterlerini_artarak_yazdirmak/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Metin giriniz: ");
-string metin =Console.ReadLine();
-
-metin = metin.ToUpper();
-
-for (int i = 0; i <= metin.Length; i++)
-    Console.WriteLine(metin.Substring(0,i));
-
-Console.ReadLine();
